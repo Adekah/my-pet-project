0858a0304d2701d26092a834a5cdaddabc8b8a51
adeka <adekahuseyin@hotmail.com>
2020-07-16 01:22:06 +0300

    Project start
    Project start

---
+++ src/main/java/com/adekah/mypetproject/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.adekah.mypetproject.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
+@Data
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateAt;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+}
---
+++ src/main/java/com/adekah/mypetproject/entity/User.java
@@ -0,0 +1,36 @@
+package com.adekah.mypetproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "email", length = 70, unique = true)
+    private String email;
+
+    @Column(name = "password", length = 200)
+    private String password;
+
+    @Column(name = "user_name", length = 100, unique = true)
+    private String username;
+
+    @Column(name = "name", length = 100)
+    private String name;
+
+    @Column(name = "surname", length = 100)
+    private String surname;
+
+}
