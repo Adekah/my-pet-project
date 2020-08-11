2ed5f2b20e835ae4b15adfde36633d2e01a5a3d9
adeka <adekahuseyin@hotmail.com>
2020-08-12 01:21:21 +0300

    QuizController-1
    QuizController-1

--- src/main/java/com/adekah/mypetproject/api/QuizController.java
+++ src/main/java/com/adekah/mypetproject/api/QuizController.java
@@ -1,13 +1,33 @@
 package com.adekah.mypetproject.api;
 
+import com.adekah.mypetproject.dto.QuizDto;
+import com.adekah.mypetproject.service.impl.QuizServiceImpl;
+import com.adekah.mypetproject.util.ApiPaths;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@Api(description="Quiz Apis")
-@RequestMapping("/api/quiz")
+@Api(description = "Quiz Apis")
+@RequestMapping(ApiPaths.QuizController.CTRL)
 public class QuizController {
+
+    private final QuizServiceImpl quizServiceImpl;
+    private final ModelMapper modelMapper;
+
+    public QuizController(QuizServiceImpl quizServiceImpl, ModelMapper modelMapper) {
+        this.quizServiceImpl = quizServiceImpl;
+        this.modelMapper = modelMapper;
+    }
+
+    @PostMapping
+    @ApiOperation(value = "Create Operation", response = QuizDto.class)
+    public ResponseEntity<QuizDto> createQuiz(@Valid @RequestBody QuizDto quiz) {
+        return ResponseEntity.ok(quizServiceImpl.save(quiz));
+    }
 }
--- src/main/java/com/adekah/mypetproject/service/impl/QuizServiceImpl.java
+++ src/main/java/com/adekah/mypetproject/service/impl/QuizServiceImpl.java
@@ -1,4 +1,46 @@
 package com.adekah.mypetproject.service.impl;
 
-public class QuizServiceImpl {
+import com.adekah.mypetproject.dto.QuizDto;
+import com.adekah.mypetproject.entity.Quiz;
+import com.adekah.mypetproject.repository.QuizRepository;
+import com.adekah.mypetproject.service.QuizService;
+import com.adekah.mypetproject.util.TPage;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.awt.print.Pageable;
+
+@Service
+public class QuizServiceImpl  implements QuizService {
+
+    private final QuizRepository quizRepository;
+    private final ModelMapper modelMapper;
+
+    public QuizServiceImpl(QuizRepository quizRepository, ModelMapper modelMapper) {
+        this.quizRepository = quizRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public QuizDto save(QuizDto quiz) {
+        Quiz q = modelMapper.map(quiz, Quiz.class);
+        q = quizRepository.save(q);
+        quiz.setId(q.getId());
+        return quiz;
+    }
+
+    @Override
+    public QuizDto getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public TPage<QuizDto> getAllPageable(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public QuizDto getByName(String name) {
+        return null;
+    }
 }
